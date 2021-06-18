93b4e982a99448c29c842266c1d2a110c833caaa
payneb35 <payne_bramyn35@yahoo.com>
2021-06-18 08:05:14 -0400

    clean up code and fix home page

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const data = require('./data.json');
-//const views = require('./views');
+const data = require('./data.json').projects;
 
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use('/static', express.static('public'));
+app.use('/static', express.static('images'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { data });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => {
